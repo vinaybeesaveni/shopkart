b05f83ec8a8c10ff68857dbfdac5e1a4a93a85e3
vinay <vinaybeesaveni@gmail.com>
2022-10-20 15:47:57 +0530

    Add HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import axios from "axios";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -14,9 +14,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
